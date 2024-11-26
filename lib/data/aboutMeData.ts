f20233105eab1eb6c99f18155072278fbb1a1b1d
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2024-11-26 21:00:35 +0200

    improve texts

--- lib/data/aboutMeData.ts
+++ lib/data/aboutMeData.ts
@@ -1,4 +1,8 @@
 export const aboutMeData = {
-  title: "About Me",
-  description: "Hi! I'm a highly driven Senior Frontend Developer with over 7 years of expertise building enterprise-grade applications. I have an eye for good design and user experience, and I've built apps for web, desktop, mobile, and XR platforms. I specialize in frontend frameworks such as React, Flutter, and Unity, but I'm also adept at full-stack tasks when required. My experience spans situational awareness software, augmented reality apps, generative AI, games, mobile and web apps, and digital twins. I help teams ship software to production with determination, focusing on creating responsive and accessible UX."
+  title: "Hey visitor!",
+  description: "Welcome to my homepage! I'm an experienced software developer with over 8 years of expertise building enterprise applications. " +
+      "I consider myself first and foremost a product engineer. I have an eye for good design and user experience, and I've built apps for web, desktop, mobile, and XR platforms. " +
+      "I specialize in frontend development utilizing frameworks such as Next.js/React, Flutter, Vue, SwiftUI and Unity, but I'm also adept at full-stack tasks. " +
+      "My experience spans situational awareness software, augmented reality apps, generative AI, games, mobile and web apps, and digital twins. " +
+      "I help teams ship software to production with determination, focusing on awesome UX/UI. I want to make a positive impact to the world and be useful."
 }; 
