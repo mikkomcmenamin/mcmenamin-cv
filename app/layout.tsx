95258ab4a6b626fca27d4a53cf0740246bc6220c
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2024-11-28 19:38:50 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import './globals.css';
 import { Space_Grotesk } from 'next/font/google';
+import { Metadata } from 'next';
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Mikko McMenamin',
   description: 'Senior Software Developer',
+  icons: {
+    icon: [{ url: '/favicon.svg', type: 'image/svg+xml' }],
+  },
 };
 
 export default function RootLayout({
@@ -15,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head />
       <body
         className={`${spaceGrotesk.className} bg-gradient-to-b from-black from-5% via-gray-950 via-40% to-black text-white`}
       >
