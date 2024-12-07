48acf7c51522bb68048924064d68f01d33696cac
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2024-12-07 17:11:18 +0200

    remove head from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,13 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        />
-          <title>Mikko McMenamin</title>
-      </head>
       <body
         className={`${spaceGrotesk.className} overflow-x-hidden bg-gradient-to-b from-black from-5% via-gray-950 via-40% to-black text-white`}
       >
